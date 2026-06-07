cb0fa37c5e5362b16130922fc72912ded9a8a256
mdmx <143160694+Madmax-xhyyy@users.noreply.github.com>
2026-06-08 02:03:32 +0800

    remove comment

--- api/src/index.ts
+++ api/src/index.ts
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Just use 'dev' logging locally—it's clean, color-coded, and single-line!
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 } else {
